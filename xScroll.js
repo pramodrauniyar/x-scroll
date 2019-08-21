6b86a795f1f4903ed53464c60feace96dd4ce400
pramodrauniyar <pramod.rauniyar@gmail.com>
2019-08-21 18:15:03 +0200

    Added possibility to added multuple slider with navigation fixes

--- xScroll.js
+++ xScroll.js
@@ -1,10 +1,10 @@
 /*global $, jQuery*/
 /**
- * Version 1.0.0
+ * Version 1.1.0
  * JQUERY plugin for horizontal scroll (xScroll)
- * @author Pramod Rauniyar 
+ * @author Pramod Rauniyar
  * @descriptions
-        simple jquery plugin for horizontal scroll
+ simple jquery plugin for horizontal scroll
 
  */
 
@@ -13,7 +13,7 @@
     $.xScroll = function(container, options) {
         //initializing variables
 
-        var $container = $(container);
+        var $container = $(options.containerWrapper +' '+container);
         var numberOfItem;
         var itemWidthPX;
         var itemWidth;
@@ -27,14 +27,15 @@
         var scrollPerItem = 2;
         var finalScroll;
 
-      
+
+
 
         //setting default variables
         var settings = $.extend({
             'containerWrapper': '',
             'animateTime' :1000,
             'btnColor':'#e03131',
-            'sliderItem':'.x-scroll-item',
+            'sliderItem':options.containerWrapper+' .x-scroll-item',
             'responsive':1,
             'responsiveItem' :2,
             'itemWidth' : 167,
@@ -44,18 +45,19 @@
         //binding the click events
         var bindEvents = function() {
 
-            //next click button 
+            //next click button
             $elementNextBtn.click(function(event) {
 
                 numberOfItem = $(settings.sliderItem).length;
-                var itemWidth = parseInt($('.x-scroll-item').css('width'));
-                var currentVisibleWidth = parseInt($('.x-scroll-jq').css('width')) + settings.itemOffset;
+                var itemWidth = parseInt($(settings.sliderItem).css('width'));
+                var currentVisibleWidth = parseInt($(settings.containerWrapper+ ' .x-scroll-jq').css('width')) + settings.itemOffset;
+
                 var visibleItemWidth = numberOfItem * itemWidth;
 
                 settings.responsive = ( visibleItemWidth > currentVisibleWidth) ? settings.responsive : 0;
-              
+
                 totalPagination = Math.ceil(numberOfItem / itemPerView)+ settings.responsive;
-             
+
                 if (currentScrollNumber === totalPagination) {
                     $elementNextBtn.addClass('disabled-btn');
                     event.stopImmediatePropagation();
@@ -63,13 +65,11 @@
                 }
                 $elementNextBtn.removeClass('disabled-btn');
                 $elementPrevBtn.removeClass('disabled-btn');
-             
+
                 finalScroll = itemWidth * currentScrollNumber * scrollPerItem;
                 $container.animate({ scrollLeft: finalScroll }, settings.animateTime);
                 currentScrollNumber = currentScrollNumber + 1;
-                var r = '-' + finalScroll + 'px';
-                var l = finalScroll + 'px';
-               
+
                 event.stopImmediatePropagation();
                 return false;
             });
@@ -77,7 +77,7 @@
 
             //right click
             $elementPrevBtn.click(function(event) {
-                console.log("page: "+ totalPagination + " numberOfItem: "+ numberOfItem +" itemPerView "+itemPerView);
+                // console.log("page: "+ totalPagination + " numberOfItem: "+ numberOfItem +" itemPerView "+itemPerView);
                 if (currentScrollNumber === 1) {
                     $elementPrevBtn.addClass('disabled-btn');
                     event.stopImmediatePropagation();
@@ -87,15 +87,10 @@
                 currentScrollNumber = currentScrollNumber - 1;
                 $elementPrevBtn.removeClass('disabled-btn');
                 $elementNextBtn.removeClass('disabled-btn');
-                //$elementNextBtn.attr('style','background:'+settings.btnColor+'');
                 finalScroll = (currentScrollNumber === 1) ? 0 : itemWidth * currentScrollNumber;
 
                 $container.animate({ scrollLeft: finalScroll }, settings.animateTime);
-               
 
-                var r = '-' + finalScroll + 'px';
-                var l = finalScroll + 'px';
-               
                 event.stopImmediatePropagation();
                 return false;
             });
@@ -104,15 +99,15 @@
         //initalizing the slider
         var init = function() {
             $(settings.containerWrapper).prepend('<div class="scroll-previous-wrapp"><a class="scroll-previous" href="#"><i class="material-icons">keyboard_arrow_left</i></a></div>');
-            $(settings.containerWrapper).prepend('<div class="scroll-next-wrapp"><a class="scroll-next" href="#"><i class="material-icons">keyboard_arrow_right</i></a></div>');
+            $(settings.containerWrapper).prepend('<div class="scroll-next-wrapp"><a class="scroll-next" href="#" ><i class="material-icons">keyboard_arrow_right</i></a></div>');
             $container.attr('style', 'position:relative');
             $elementNextBtnWrapper = $('.scroll-next-wrapp');
             $elementPreviousBtnWrapper = $('.scroll-previous-wrapp');
-            $elementNextBtn = $('.scroll-next');
-            $elementPrevBtn = $('.scroll-previous');
+            $elementNextBtn = $(options.containerWrapper +' .scroll-next');
+            $elementPrevBtn =  $(options.containerWrapper +' .scroll-previous');
             $elementNextBtn.attr('style','background:'+settings.btnColor+'');
             $elementPrevBtn.attr('style','background:'+settings.btnColor+'');
-           // $elementPrevBtn.addClass('disabled-btn');
+            // $elementPrevBtn.addClass('disabled-btn');
             itemWidthPX = $(settings.sliderItem).css('width');
             itemWidth = parseInt(itemWidthPX);
             itemPerView = Math.round(parseInt($container.css('width')) / itemWidth);
@@ -120,7 +115,12 @@
             bindEvents();
         };
         init();
-       
+
 
     };
 }(jQuery));
+// var scrollContainer=".x-scroll-jq";
+// $.xScroll(scrollContainer,{containerWrapper:"#x-scroll-jq"});
+//
+//
+// $.xScroll(scrollContainer,{containerWrapper:"#x-scroll-jq-2"});
